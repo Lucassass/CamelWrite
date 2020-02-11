fafbb34a5741384df19f91e0594e420644959102
lucas sass <lucassass@live.com>
2020-02-11 15:47:32 +0100

    :+1:

---
+++ README.md
@@ -0,0 +1,34 @@
+@author Lucas Sass Rósinberg 
+
+camelWriter is a solution to an Exercise that was given 
+to me during one of my one of my 2. semester of courses 
+in software engineering at SDU.
+
+
+The requirement for the assignment was:
+
+The first char in the first word in a line had to be lower case
+
+The following word in the line the first char had to be upper case
+
+The words had to be put together without any spaces in between
+
+
+camelWriter is a program that converts txt files line by line 
+to lowerCamel Case also known as mixed camel case. 
+for camelWriter to work the txt file has to be the project folder 
+i have included some examples of 3 songs which i got for the exercise
+
+
+this means that if you have a String like:
+
+"Hello my name is lucas"
+
+it would convert it to 
+
+"helloMyNameIsLucas"
+
+
+once the entire file has been converted, a new file will be
+made in the folder with the containing the changes called 
+camelWrite.txt 
---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.io.*;
+
+
+class CamelWriter {
+
+    // converts Strings to lowerCamel case
+    private static String toLowerCamelCase(String line) {
+        String result = "";
+        if (line.length() == 0) {
+            return result;
+        }
+        char firstChar = line.charAt(0);
+        char firstCharToLowerCase = Character.toLowerCase(firstChar);
+        result = result + firstCharToLowerCase;
+        for (int i = 1; i < line.length(); i++) {
+            char currentChar = line.charAt(i);
+            char previousChar = line.charAt(i - 1);
+            if (previousChar == ' ') {
+                char currentCharToUpperCase = Character.toUpperCase(currentChar);
+                result = result + currentCharToUpperCase;
+            } else {
+                char currentCharToLowerCase = Character.toLowerCase(currentChar);
+                result = result + currentCharToLowerCase;
+                result = result.replaceAll("\\s", "");
+            }
+        }
+        return result;
+    }
+
+    // Read the file lines by lines and calls the methode convert2camel
+    private void readLines() {
+        BufferedReader reader;
+        try {
+            //insert the file that need to be converted here
+            reader = new BufferedReader(new FileReader("MaryAnn.txt"));
+            String line = reader.readLine();
+            while( line!= null ){
+                convert2camel(line);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // convert2camel tells what to do with the each line
+    private void convert2camel(String line) {
+        // if the line is empty print the line because it means that it is an separation
+        if (line == null || line.isEmpty()) {
+            System.out.println(line);
+        }
+        // if the line contains text we tell it to preform the conversion to lowerCamelCase
+        else {
+            System.out.println(toLowerCamelCase(line));
+        }
+    }
+    public static void main(String args[]) throws FileNotFoundException {
+        // creates new file camelWriter.txt and inputs the conversions into the new file
+        PrintStream fileOut = new PrintStream("./camelWriter.txt");
+        System.setOut(fileOut);
+        CamelWriter cw = new CamelWriter();
+        cw.readLines();
+    }
+}
